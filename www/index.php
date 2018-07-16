df8788d05902e7a3031a3d7203c5510469c4075c
Marty Pavalko <martyp41@gmail.com>
2018-07-16 15:33:18 -0400

    Initial Commit

---
+++ www/index.php
@@ -0,0 +1,18 @@
+<?php echo "Hello There! <br>" ?>
+
+<?php
+    $con = mysqli_connect('db', 'root', 'password', null);
+
+    if (!$con)
+    {
+        echo "Error: Unable to connect to MySQL." . PHP_EOL;
+        echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+    
+    echo "Success: A proper connection to MySQL was made! The docker database is great." . PHP_EOL;
+    echo "Host information: " . mysqli_get_host_info($con) . PHP_EOL;
+    
+    mysqli_close($con);
+?>
